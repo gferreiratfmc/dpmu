6b7977ef3f0317af4e41967dc8556bc5d923518b
Gustavo Ferreira <gustavo.ferreira2@technipfmc.com>
2024-05-29 16:00:01 -0300

    Stop python GUI before updating the firware and restart after the update

--- dpmu_cpu1/crc_CPU1_FLASH_NODEBUG.bat
+++ dpmu_cpu1/crc_CPU1_FLASH_NODEBUG.bat
@@ -40,6 +40,8 @@ dir %TARGET%
 
 pause
 
+taskkill /IM python.exe /F
+
 cd "C:\Users\gferreira\OneDrive - Digicorner\git_dpmu_python\a055-can-updater\"
 
 python CANUpdaterEmotasOri.py 1 "C:\Users\gferreira\workspace_endurance_cllc\dpmu_cpu1\CPU1_FLASH_NODEBUG\dpmu_cpu1.crc"
